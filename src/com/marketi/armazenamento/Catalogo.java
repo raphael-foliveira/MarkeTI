70c29a1094f2a55a4790166ef938c6bd3ccc2e01
raphael <raphael.foliveira1@gmail.com>
2022-05-06 22:40:42 -0300

    criar-catalogo

---
+++ src/com/marketi/armazenamento/Catalogo.java
@@ -0,0 +1,12 @@
+package com.marketi.armazenamento;
+
+import java.util.ArrayList;
+import com.marketi.entidades.*;
+
+public class Catalogo {
+    ArrayList<Produto> produtos;
+
+    Catalogo(){
+        produtos = new ArrayList<Produto>();
+    }
+}
