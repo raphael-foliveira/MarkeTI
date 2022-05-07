31a8d130be93bea222c7500a0d8db23018f01435
raphael <raphael.foliveira1@gmail.com>
2022-05-06 22:56:23 -0300

    reimplement-id-add-remove-add

--- src/com/marketi/armazenamento/Catalogo.java
+++ src/com/marketi/armazenamento/Catalogo.java
@@ -9,4 +9,21 @@ public class Catalogo {
     Catalogo(){
         produtos = new ArrayList<Produto>();
     }
+
+    public void adicionar(Produto produto){
+        produtos.add(produto);
+    }
+
+    public void remover(Produto produto){
+        produtos.remove(produto);
+    }
+
+    public void remover(String id) {
+        for (Produto produto : produtos) {
+            if (produto.getId().equals(id)){
+                produtos.remove(produto);
+                return;
+            }
+        }
+    }
 }
--- src/com/marketi/entidades/Produto.java
+++ src/com/marketi/entidades/Produto.java
@@ -2,18 +2,46 @@ package com.marketi.entidades;
 
 
 public class Produto {
-    String marca, modelo, lote;
+    String id, marca, modelo, lote;
     double preco;
 
     public Produto(
+        String id,
         String marca, 
         String modelo,
         String lote, 
         double preco
     ) {
+        this.id = id;
         this.marca = marca;
         this.modelo = modelo;
         this.lote = lote;
+        this.preco = preco;
+    }
+    
+    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLote() {
+        return lote;
+    }
+
+    public void setLote(String lote) {
+        this.lote = lote;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
         this.preco = preco;
     }
 
